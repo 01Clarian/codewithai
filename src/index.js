8ab0ded2ab7ebde10e916a2d6602a0f65b2720a4
01clarian <clarionnorth@gmail.com>
2023-03-31 09:46:00 -0400

    routing hash

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, HashRouter } from "react-router-dom";
 import App from "./App";
 
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -8,7 +8,9 @@ import "./assets/css/index.css";
 import "./assets/scss/light-bootstrap-dashboard-react.scss?v=2.0.0";
 
 createRoot(document.getElementById("root")).render(
+  <HashRouter>
     <Router>
     <App />
   </Router>
+  </HashRouter>
 );
